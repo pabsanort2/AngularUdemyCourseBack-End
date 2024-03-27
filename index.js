b0b8f18db1765d3505e6b09a8a2e0ff39992a19e
pabsanort2 <pabsanort2@alum.us.es>
2024-03-27 12:36:29 +0100

    Added a listen poin on the server with the 4000 port and a get route for a hello world message

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("desde index.js")
+const express = require("express");
+
+// Create the express server
+const app = express();
+
+// Define route
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(4000, () => {
+    console.log("El servidor esta corriendo correctamente")
+})
