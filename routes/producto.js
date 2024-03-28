f43212e77fd7269fe97b5a73620dea2b8b24bbfb
pabsanort2 <pabsanort2@alum.us.es>
2024-03-28 11:19:59 +0100

    Created a controller for the products and imported it in the product route file

--- routes/producto.js
+++ routes/producto.js
@@ -2,10 +2,9 @@
 
 const express = require("express");
 const router = express.Router();
+const productoController = require("../controllers/productoController")
 
 // api/productos
-router.post("/", () => {
-    console.log("Creando producto...")
-})
+router.post("/", productoController.crearProducto)
 
 module.exports = router;
